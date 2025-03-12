315242dd190ca68fc8059f4e40ab9413566c3084
caresys-mark <mark.huang@caresys.com.tw>
2025-03-12 17:32:43 +0800

    建立專案

---
+++ src/test/java/com/example/demo/CoindeskBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindeskBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
